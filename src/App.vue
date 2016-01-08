1dc9b29f0d6a74057e4d312762979762c9e52348
lishengzxc <eric@lishengcn.cn>
2016-01-08 23:24:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <todo-form></todo-form>
+  </div>
+</template>
+
+<script>
+import TodoForm from './components/todo-form.vue'
+
+export default {
+  components: {
+    'todo-form': TodoForm
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/todo-form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>{{todo}}</div>
+    <input type="text" v-model="todo">
+    <button v-on:click="addTodo">submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todo: ''
+    }
+  },
+  methods: {
+    addTodo () {
+      alert(this.todo)
+      this.todo = ''
+    }
+  }
+}
+</script>
